174824dfcf25913fd9d3b8950b8fcdf7885441c6
Aster Marias <aster@slware.org>
2026-07-27 08:40:05 -0600

    Added support for writing Nix

--- init.lua
+++ init.lua
@@ -679,6 +679,7 @@ require('lazy').setup({
         clangd = {},
         -- gopls = {},
         pyright = {},
+        nil_ls = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -721,6 +722,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'nixfmt',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
